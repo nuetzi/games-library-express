2e178d11c2beb54026a3ac993df3ffef82790ba7
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-04-30 23:00:01 -0400

    Updated games model

--- models/games.js
+++ models/games.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const gamesSchema = new mongoose.Schema({
     title: {type: String, required: true},
+    genres: {type: String, required: true},
     year: {type: Number, required: true},
     cover: {type: String, required: true},
     rating: {type: Number},
