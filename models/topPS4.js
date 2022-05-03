15e4be335900748e2ed514578b72485b87c59319
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-05-03 09:47:41 -0400

    Playstation4 update

---
+++ models/topPS4.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const topPS4Schema = new mongoose.Schema({
+    id: Number,
+    aggregated_rating: Number,
+    cover: {
+        id: Number,
+        url: String
+    },
+    first_release_date: Number,
+    genres: [{
+        id: Number,
+        name: String
+    }],
+    involved_companies: [{
+        id: Number,
+        company: {
+            id: Number,
+            name: String
+        }
+    }],
+    name: String,
+    platforms: [{
+        id: Number,
+        name: String
+    }],
+    rating: Number,
+    storyline: String,
+    summary: String,
+    total_rating: Number,
+    url: String
+});
+
+const TopPS4 = mongoose.model("TopPS4", topPS4Schema, "topPS4");
+module.exports = TopPS4;
