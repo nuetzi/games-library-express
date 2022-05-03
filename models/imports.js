f2c3d9578cf25169a1a3741e0476cea182a7aa5a
Marcus Nuetzi <97974083+Nuetzi@users.noreply.github.com>
2022-05-02 21:19:48 -0400

    Trying to import external data from IGDB

--- models/imports.js
+++ models/imports.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 
 const importsSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    genres: {type: String, required: true},
-    year: {type: Number, required: true},
-    cover: {type: String, required: true},
+    id: {type: Number, required: true},
+    aggregated_rating: {type: Number},
+    cover: {type: Object, required: true},
+    first_release_date: {type: Number, required: true},
+    genres: {type: Array, required: true},
+    involved_companies: {type: Array, required: true},
+    name: {type: String, required: true},
+    platforms: {type: Array, required: true},
     rating: {type: Number},
-    platforms: {type: String, required: true},
     summary: {type: String, required: true},
+    total_rating: {type: Number},
     url: {type: String}
 });
 
